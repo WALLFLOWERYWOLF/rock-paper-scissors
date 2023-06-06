b0b381444fed18a90c7915d978bbd412fc7fdcd0
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2023-06-06 12:35:41 +0530

    feat: add function for random computer choice
    Added a function that will randomly return either
    ‘Rock’, ‘Paper’ or ‘Scissors’.

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "paper";
+      break;
+  }
+}
