de7b5c7c1198ccf4dfc89f5dac4bebdbc3533f30
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2023-06-08 17:14:50 +0530

    feat: add function that plays a round of RPS
    Added a function that plays a single round of Rock Paper & Scissor
    and declares the result.

--- script.js
+++ script.js
@@ -13,3 +13,52 @@ function getComputerChoice() {
       break;
   }
 }
+
+function playRound(playerChoice, computerChoice) {
+  let playerChoiceCaseInsensitive = playerChoice.toLowerCase();
+  if (playerChoiceCaseInsensitive === "rock" && computerChoice === "rock") {
+    return "Tie";
+  } else if (
+    playerChoiceCaseInsensitive === "rock" &&
+    computerChoice === "paper"
+  ) {
+    return "You Lose! Paper beats Rock.";
+  } else if (
+    playerChoiceCaseInsensitive === "rock" &&
+    computerChoice === "scissors"
+  ) {
+    return "You Win! Rock beats Scissors.";
+  } else if (
+    playerChoiceCaseInsensitive === "paper" &&
+    computerChoice === "rock"
+  ) {
+    return "You Win! Paper beats Rock.";
+  } else if (
+    playerChoiceCaseInsensitive === "paper" &&
+    computerChoice === "paper"
+  ) {
+    return "Tie";
+  } else if (
+    playerChoiceCaseInsensitive === "paper" &&
+    computerChoice === "scissors"
+  ) {
+    return "You Lose! Scissors beat Paper.";
+  } else if (
+    playerChoiceCaseInsensitive === "scissors" &&
+    computerChoice === "rock"
+  ) {
+    return "You Lose! Rock beats Scissors.";
+  } else if (
+    playerChoiceCaseInsensitive === "scissors" &&
+    computerChoice === "paper"
+  ) {
+    return "You Win! Scissors beat Paper.";
+  } else if (
+    playerChoiceCaseInsensitive === "scissors" &&
+    computerChoice === "scissors"
+  ) {
+    return "Tie";
+  } else {
+    return "Invalid choice! Please choose Rock, Paper or Scissors.";
+  }
+}
