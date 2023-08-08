e354f74df0e208ed29d429c4b102add0fc8bd1d5
WALLFLOWERYWOLF <121654472+WALLFLOWERYWOLF@users.noreply.github.com>
2023-08-08 10:22:51 +0530

    feat: add a UI to the game

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector(".results");
+const roundResultAnnouncement = document.createElement("div");
+const playerScoreAnnouncement = document.createElement("div");
+const computerScoreAnnouncement = document.createElement("div");
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const roundResult = playRound(
+      e.target.classList.value,
+      getComputerChoice()
+    );
+    roundResultAnnouncement.textContent = roundResult;
+    results.appendChild(roundResultAnnouncement);
+
+    switch (roundResult) {
+      case "You Win! Rock beats Scissors.":
+      case "You Win! Paper beats Rock.":
+      case "You Win! Scissors beat Paper.":
+        playerScore++;
+        break;
+      case "You Lose! Paper beats Rock.":
+      case "You Lose! Scissors beat Paper.":
+      case "You Lose! Rock beats Scissors.":
+        computerScore++;
+        break;
+    }
+
+    playerScoreAnnouncement.textContent = `YOUR SCORE: ${playerScore}`;
+    results.appendChild(playerScoreAnnouncement);
+
+    computerScoreAnnouncement.textContent = `COMPUTER SCORE: ${computerScore}`;
+    results.appendChild(computerScoreAnnouncement);
+
+    if (playerScore === 5 || computerScore === 5) {
+      if (playerScore === 5) {
+        const playerWinner = document.createElement("div");
+        playerWinner.textContent = `Congratulations! You've won the game. Your Score: ${playerScore} Computer Score: ${computerScore}`;
+        results.appendChild(playerWinner);
+      } else {
+        const computerWinner = document.createElement("div");
+        computerWinner.textContent = `Alas! You've lost the game. Your Score: ${playerScore} Computer Score: ${computerScore}`;
+        results.appendChild(computerWinner);
+      }
+      playerScore = 0;
+      computerScore = 0;
+      playerScoreAnnouncement.textContent = `YOUR SCORE: ${playerScore}`;
+      computerScoreAnnouncement.textContent = `COMPUTER SCORE: ${computerScore}`;
+    }
+  });
+});
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
 
@@ -15,7 +69,6 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-  playerChoice = playerChoice.toLowerCase();
   if (playerChoice === "rock" && computerChoice === "rock") {
     return "Tie";
   } else if (playerChoice === "rock" && computerChoice === "paper") {
@@ -34,50 +87,5 @@ function playRound(playerChoice, computerChoice) {
     return "You Win! Scissors beat Paper.";
   } else if (playerChoice === "scissors" && computerChoice === "scissors") {
     return "Tie";
-  } else {
-    return "Invalid choice! Please choose Rock, Paper or Scissors.";
   }
 }
-
-function game() {
-  let playerChoice;
-  let computerChoice;
-  let playerScore = 0;
-  let computerScore = 0;
-  let roundResult;
-
-  for (let i = 0; i < 5; i++) {
-    playerChoice = prompt("Choose Rock, Paper or Scissors");
-    computerChoice = getComputerChoice();
-    roundResult = playRound(playerChoice, computerChoice);
-    console.log(roundResult);
-
-    switch (roundResult) {
-      case "You Win! Rock beats Scissors.":
-      case "You Win! Paper beats Rock.":
-      case "You Win! Scissors beat Paper.":
-        playerScore++;
-        break;
-      case "You Lose! Paper beats Rock.":
-      case "You Lose! Scissors beat Paper.":
-      case "You Lose! Rock beats Scissors.":
-        computerScore++;
-        break;
-    }
-  }
-  if (playerScore > computerScore) {
-    console.log("Congratulations! You've won the game.");
-    console.log(`YOUR SCORE: ${playerScore}`);
-    console.log(`COMPUTER SCORE: ${computerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log("Alas! You've lost the game.");
-    console.log(`YOUR SCORE: ${playerScore}`);
-    console.log(`COMPUTER SCORE: ${computerScore}`);
-  } else {
-    console.log("The game ended in a tie.");
-    console.log(`YOUR SCORE: ${playerScore}`);
-    console.log(`COMPUTER SCORE: ${computerScore}`);
-  }
-}
-
-game();
